e5c911dfd282ce499c8082605244391af85ffce5
satindersingh772 <92008199+satindersingh772@users.noreply.github.com>
2022-10-21 23:50:24 -0600

    worked_with_error_handling

--- apihelpers.py
+++ apihelpers.py
@@ -9,18 +9,8 @@ import json
 # single function to handle database interactions
 # and based on results will send errors
 def get_display_results(statement,args_list):
-    results = conn_exe_close(statement,args_list)
-    if(type(results) == list):
-        if(len(results) == 0):
-            return 'No Results matched your input'
-        return results
-    elif(type(results) == str):
-        if(results.startswith('Duplicate entry')):
-            return 'Duplicate data already exists, please change values and try again'
-        return make_response(json.dumps(results, default=str), 400)
-    else:
-        return make_response(json.dumps(results, default=str) , 500)
-
+    return conn_exe_close(statement,args_list)
+    
 # will verifiy end points arguments for presence
 # if necessary arguments not sent then remind the user to send
 def verify_endpoints_info(sent_data,required_args):
--- clients.py
+++ clients.py
@@ -7,24 +7,40 @@ from uuid import uuid4
 
 # specific client will return the client with the provided input id
 def specific_client():
+    # check for client id is given or no
     invalid = verify_endpoints_info(request.args,['client_id'])
     if(invalid != None):
+        # if not sent as a param then it will show an error
         return make_response(json.dumps(invalid,default=str),400)
+        # is sent then it will go further and call the function to add client wit given all 
+        # the data inside params
     results = get_display_results('call specific_client(?)',[request.args.get('client_id')])
-    results_json = make_response(json.dumps(results,default=str),200)
-    return results_json
+    if(type(results) == list):
+        return make_response(json.dumps(results,default=str),200)
+    elif(type(results) == str):
+        return make_response(json.dumps(results,default=str),400)
+    else:
+        return make_response(json.dumps(results,default=str),500)
 
 # add client will add the new client to the database and return the id of newly inserted client
 def add_client():
+    # will check if all the required params are sent
     invalid = verify_endpoints_info(request.json,['username','first_name','last_name','email','image_url','password'])
     if(invalid != None):
+        # if not then it will ask for to send
         return make_response(json.dumps(invalid,default=str),400)
+        # if sent then it will generate a token and send it along with
+        # other data params to the stored procedure
     token = uuid4().hex
     results = get_display_results('call add_client(?,?,?,?,?,?,?)',
     [request.json.get('username'),request.json.get('first_name'),request.json.get('last_name'),request.json.get('email'),
     request.json.get('image_url'),request.json.get('password'),token])
-    results_json = make_response(json.dumps(results,default=str),200)
-    return results_json
+    if(type(results) == list):
+        return make_response(json.dumps(results,default=str),200)
+    elif(type(results) == str):
+        return make_response(json.dumps(results,default=str),400)
+    else:
+        return make_response(json.dumps(results,default=str),500)
 
 def token_valid():
     invalid = verify_endpoints_info(request.headers,['token'])
@@ -42,14 +58,18 @@ def client_delete():
             return make_response(json.dumps(invalid,default=str),400)
         results = get_display_results('call client_delete(?,?)',
         [id[0][0],request.json.get('password')])
-        if(results[0][0] == 1):
+        if( type(results) == list and results[0][0] == 1):
             results_json = make_response(json.dumps('Client deleted successfully',default=str),200)
             return results_json
-        elif(results[0][0] == 0):
+        elif( type(results)== list and results[0][0] == 0):
             return make_response(json.dumps('No client deleted',default=str),400)
+        elif(type(results) != list):
+            return make_response(json.dumps(results,default=str),400)
         else:
             return make_response(json.dumps(results,default=str),500)
     elif(type(id) == list and id[0][0] != int and id[0][1] == 0):
         return make_response(json.dumps('No user exists with these credentials',default=str),400)
+    elif(type(id) != list or type(id) == str):
+        return id
     else:
-        return make_response(json.dumps('Not a valid token',default=str),400)
+        return make_response(json.dumps('Sorry, something went wrong',default=str),500)
