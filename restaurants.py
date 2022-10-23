d04fc8f2b5fe9c36809e408eb857c254cb16f4dd
satinder <satindersingh472@gmail.com>
2022-10-23 07:00:36 -0600

    restaurant_login_comments_done

--- restaurants.py
+++ restaurants.py
@@ -5,33 +5,53 @@ from uuid import uuid4
 from dbhelpers import conn_exe_close
 from apihelpers import verify_endpoints_info
 
+
+# will login the restaurant and return the id and token for the restaurant
 def restaurant_login():
+    # check if email and password are present in the request
     invalid = verify_endpoints_info(request.json, ['email','password'])
     if(invalid != None):
+        # if not then return the error for missing data
         return make_response(json.dumps(invalid,default=str),400)
+    # will create the token if data is present
     token = uuid4().hex
+    # will make the request to database to add a token and id to the restaurant session
     results = conn_exe_close('call restaurant_login(?,?,?)',
     [request.json.get('email'),request.json.get('password'),token])
+    # results will return the id and token if all goes fine
+    # in a form of list
     if(type(results) == list and len(results) == 1):
         return make_response(json.dumps(results,default=str),200)
+        # if something is wrong then error will get returned
     elif(type(results) == list and len(results) == 0):
         return make_response(json.dumps('Invalid username or password',default=str),400)
     elif(type(results) == str):
+        # if there is any other error related to user then the error will be presented to the user
         return make_response(json.dumps(results,default=str),400)
     else:
+        # or if there is any server error it will show up as error 500
         return make_response(json.dumps(results,default=str),500)
     
 
+# will delete the token sent to the database from restaurant sessions
+# and return the row count which should be one if all goes well
 def restaurant_logout():
+    # will check for the headers if token is sent
     invalid_header = verify_endpoints_info(request.headers,['token'])
     if(invalid_header != None):
+        # if token not sent then error will be asking for token to send
         return make_response(json.dumps(invalid_header,default=str),400)
+        # if token is sent then will request the database to process it further and delete the specific token from db
     results = conn_exe_close('call restaurant_logout(?)',[request.headers.get('token')])
+    # if tokenn exists and got deleted and then row count will be sent back in a form of a list
+    # and appropriate message will be shown to the user
     if(type(results) == list and results[0][0] == 1):
         return make_response(json.dumps('restaurant logout successfully',default=str),200)
     elif(type(results) == list and results[0][0] == 0):
+        # if not successfull then error message will get displayed
         return make_response(json.dumps('restaurant logout not successfull or already logged out',default=str),400)
     else:
+        # if there is any server error it will show error 500
         return make_response(json.dumps(results,default=str),500)
 
 
