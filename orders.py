0bb481465e432e41e72316359f988ea415277ee8
satinder <satindersingh472@gmail.com>
2022-10-26 14:31:09 -0600

    commenting_order_post_done

--- orders.py
+++ orders.py
@@ -7,25 +7,38 @@ from apihelpers import verify_endpoints_info
 def client_post():
     # will check if header is sent
     invalid_header = verify_endpoints_info(request.headers,['token'])
+    # if not sent then the error will show up
     if(invalid_header != None):
         return make_response(json.dumps(invalid_header,default=str),400)
+        # check for various data arguments if not sent then an error will show up
     invalid = verify_endpoints_info(request.json,['menu_items','restaurant_id'])
     if(invalid != None):
         return make_response(json.dumps(invalid,default=str),400)
+        # will make a request to get back the order id
     order_post = conn_exe_close('call order_post(?,?)',[request.json.get('restaurant_id'),request.headers.get('token')])
+    # use the order to post a menu
     if(type(order_post) == list and len(order_post) == 1):
+        # menu items will be array that is sent with menu item ids
         menu_items = request.json.get('menu_items')
+        # will loop through menu items and send a request for each menu item with id
+        # by using order id and menu item id
         for menu_item in menu_items:
             results = conn_exe_close('call orders_menus_post(?,?)',[order_post[0][0],menu_item])
+            # after the loop finishes it will show the results as id of the order posted
         if(type(results) == list):
             return make_response(json.dumps(results,default=str),200)
         elif(type(results) == str):
+            # if error it will show the error with a code 400
             return make_response(json.dumps(results,default=str),400)
         else:
+            # if there is any other error besides user input or database the server error will show up
             return make_response(json.dumps(results,default=str),500)
     elif(type(order_post) == str):
+        # if order post not able to bring back the if of an order
+        # then this result will be shown
         return make_response(json.dumps(order_post,default=str),400)
     else:
+        # or if there is any other error wit server regarding order post call it will show this error 
         return make_response(json.dumps(order_post,default=str),500)
 
 
