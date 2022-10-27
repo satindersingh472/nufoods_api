00a1a3e1bf2e3929284ef93e89a441954a78513f
satindersingh772 <92008199+satindersingh772@users.noreply.github.com>
2022-10-26 21:59:30 -0600

    commenting_on_order_restaurant_get_confirmed_done

--- restaurant_orders.py
+++ restaurant_orders.py
@@ -18,6 +18,29 @@ def order_restaurant_get():
         return make_response(json.dumps(results,default=str),500)
 
 
+def order_restaurant_get_confirmed():
+    # check for user input for is confirmed argument 
+    # if true it will store 1 in data_one variable which means true for database
+    # if false then it will send 0 which means false for database
+    if(request.args.get('is_confirmed') in ['true','True']):
+        data_one = 1
+    elif(request.args.get('is_confirmed') in ['false','False']):
+        data_one = 0
+    results = conn_exe_close('call order_restaurant_get_confirmed(?,?)',[request.headers.get('token'),data_one])
+    # will send request and expect a list in return 
+    if(type(results) == list):
+        return make_response(json.dumps(results,default=str),200)
+    elif(type(results) == str):
+        # str of results means problem with user input
+        return make_response(json.dumps(results,default=str),400)
+    else:
+        # else if something goes wrong even after user sends correct info then it is server error
+        return make_response(json.dumps(results,default=str),500)
+
+
+
+
+
 def restaurant_get():
     # will check for header sent if not then error will show up
     invalid_header = verify_endpoints_info(request.headers,['token'])
@@ -29,3 +52,6 @@ def restaurant_get():
     # if no data argument is sent then this statement will be true and execute the function
     if(is_confirmed == None and is_completed == None):
         return order_restaurant_get()
+        # if is oonfirm is sent then the following statement will become true and execute the function
+    elif(is_confirmed != None and is_completed == None):
+        return order_restaurant_get_confirmed()
